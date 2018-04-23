116e1e6b7a6799209e40bc6ee63d652150ee0618
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_DisplayCustomText/Window1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using DevExpress.Wpf.Grid;
+using DXExample.DemoData;
+
+namespace DXGrid_DisplayCustomText {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            gridControl1.DataSource = Invoice.GetData();
+        }
+        private void gridControl1_CustomColumnDisplayText(object sender,
+                CustomColumnDisplayTextEventArgs e) {
+            if (e.Column != columnProductName) return;
+            if ((double)gridControl1.GetCellValue(e.RowHandle, "Discount") > 20)
+                e.DisplayText = ((string)e.Value) + " (SALE)";
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to display custom text within DXGrid cells
+
+
+<p>The following example shows how to provide custom display text for the DXGrid cells.</p><p>In this example, if a value in the 'Discount' column is greater than 20, the '(SALE)' string is added to the name of the product in the 'Product Name' column.</p>
+
+<br/>
+
+
