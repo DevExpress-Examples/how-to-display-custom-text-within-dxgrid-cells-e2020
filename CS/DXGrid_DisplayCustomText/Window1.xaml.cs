f7dd151b03df7eb03fc6c8780ebc225425aea24e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_DisplayCustomText/Window1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Grid;
+using DXExample.DemoData;
+
+namespace DXGrid_DisplayCustomText {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            gridControl1.ItemsSource = Invoice.GetData();
+        }
+        private void gridControl1_CustomColumnDisplayText(object sender,
+                CustomColumnDisplayTextEventArgs e) {
+            if (e.Column != columnProductName) return;
+            if ((double)gridControl1.GetCellValue(e.RowHandle, "Discount") > 20)
+                e.DisplayText = ((string)e.Value) + " (SALE)";
+        }
+    }
+}
