38f5c720651c9c3a0580670179cc075b31142d02
Alexander-Ploskin <55746901+Alexander-Ploskin@users.noreply.github.com>
2021-08-12 12:59:36 +0300

    Update MainWindow.xaml.cs

--- CS/DisplayCustomText_CodeBehind/MainWindow.xaml.cs
+++ CS/DisplayCustomText_CodeBehind/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
-using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Grid; 
 
 namespace DisplayCustomText_CodeBehind {
     public class Invoice {
