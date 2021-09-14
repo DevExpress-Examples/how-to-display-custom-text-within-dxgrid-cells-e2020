370ea45690e5109473524d859cb4b2642459e1aa
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2021-09-14 20:45:29 +0300

    Update MainViewModel.cs

--- CS/DisplayCustomText_MVVM/MainViewModel.cs
+++ CS/DisplayCustomText_MVVM/MainViewModel.cs
@@ -21,12 +21,12 @@ namespace DXGrid_DisplayCustomText_MVVM {
             InvoiceList = new ObservableCollection<Invoice>(GetData());
         }
         IEnumerable<Invoice> GetData() {
-            yield return new Invoice() { ID = 0, ProductName = "Tofu", Price = 235.54, Discount = 9.4, ShippingDate = new DateTime(2010, 3, 12), IsShipped = false };
-            yield return new Invoice() { ID = 1, ProductName = "Ravioli Angelo", Price = 178.45, Discount = 6.1, ShippingDate = new DateTime(2010, 2, 9), IsShipped = true };
-            yield return new Invoice() { ID = 2, ProductName = "Geitost", Price = 89.98, Discount = 5.4, ShippingDate = new DateTime(2010, 4, 1), IsShipped = false };
-            yield return new Invoice() { ID = 3, ProductName = "Chang", Price = 189.33, Discount = 18.2, ShippingDate = new DateTime(2010, 5, 23), IsShipped = true };
-            yield return new Invoice() { ID = 4, ProductName = "Inlagd Sill", Price = 509.10, Discount = 22.2, ShippingDate = new DateTime(2010, 6, 30), IsShipped = false };
-            yield return new Invoice() { ID = 5, ProductName = "Alice Mutton", Price = 791.18, Discount = 24.4, ShippingDate = new DateTime(2010, 5, 7), IsShipped = true };
+            yield return new Invoice() { ID = 0, ProductName = "Tofu", Price = 235.54, Discount = 9.4, ShippingDate = new DateTime(2020, 3, 12), IsShipped = false };
+            yield return new Invoice() { ID = 1, ProductName = "Ravioli Angelo", Price = 178.45, Discount = 6.1, ShippingDate = new DateTime(2020, 2, 9), IsShipped = true };
+            yield return new Invoice() { ID = 2, ProductName = "Geitost", Price = 89.98, Discount = 5.4, ShippingDate = new DateTime(2020, 4, 1), IsShipped = false };
+            yield return new Invoice() { ID = 3, ProductName = "Chang", Price = 189.33, Discount = 18.2, ShippingDate = new DateTime(2020, 5, 23), IsShipped = true };
+            yield return new Invoice() { ID = 4, ProductName = "Inlagd Sill", Price = 509.10, Discount = 22.2, ShippingDate = new DateTime(2020, 6, 30), IsShipped = false };
+            yield return new Invoice() { ID = 5, ProductName = "Alice Mutton", Price = 791.18, Discount = 24.4, ShippingDate = new DateTime(2020, 5, 7), IsShipped = true };
         }
 
         [Command]
